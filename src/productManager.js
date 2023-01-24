1830ea24edbe3efca4cac9f3f7576006346a37ad
Rodrigo Trillo <rollyt1232@gmail.com>
2023-01-24 19:23:38 -0300

    add endpoints

--- src/productManager.js
+++ src/productManager.js
@@ -1,48 +1,36 @@
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid');
-
-const products = [];
 const id = uuidv4();
-const code = uuidv4()
-class ProductManager{
 
-    
-    
-    
-  addProduct = (id,title, description, price, thumbnail, code, stock)=>{
-        products.push({
-            id: id,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
-        });
+
+
+
+class ProductManager {
+
+    constructor() {
+        this.products= []
     }
-    
-    
+
+    create({name, description, stock, price,id}){
+        const product = new Product();
+
+        Object.assign(product,{
+            name,
+            description,
+            stock,
+            price,
+            id:id,
+            created_at: new Date()
+        })
+
+        this.products.push(product)
+
+
+    }
+
+
 }
 
-let productManager = new ProductManager();
-    productManager.addProduct(id,'Producto1',"description1","$33",'img1',code,"22")
-    productManager.addProduct(id,'Producto2',"description2","$55",'img1',code,"22")
-    productManager.addProduct(id,'Producto3',"description3","$66",'img1',code,"22")
-    
-    
-    let jsonData = JSON.stringify(products);
-    
-    
-    fs.writeFile("./data/products.json", jsonData, (error) => {
-        if (error) {
-            console.log(`Error: ${error}`);
-        } else {
-            console.log("Archivo JSON creado correctamente");
-            
-        }
-  });
-
-let data=products
 
 
-module.exports = ProductManager 
+module.exports = ProductManager;
--- src/products/controller.products.js
+++ src/products/controller.products.js
@@ -1,10 +1,58 @@
 const {Router} = require('express')
-const fs =require('fs')
-const prodManag= require ('../productManager.js')
+const fs =require('fs');
+const { findAll } = require('../productManager');
+const ProductManager = require('../productManager')
 const router = Router();
+const { v4: uuidv4 } = require('uuid');
+const id = uuidv4();
 
+ const Product = [
+    {
+        "title": "prod1",
+        "stock": 121,
+        "description": "",
+        "price": 100,
+        "thumbnail": "reultrasecretofads",
+        "code": "123abdefghy",
+        "category":"Product",
+        "status":true,
+        "id": 1
+      },
+    {
+        "title": "prod2",
+        "stock": 121,
+        "description": "",
+        "price": 100,
+        "thumbnail": "reultrasecretofads",
+        "code": "123abdefghy",
+        "category":"Product",
+        "status":true,
+        "id": 2
+      },
+    {
+        "title": "prod3",
+        "stock": 121,
+        "description": "",
+        "price": 100,
+        "thumbnail": "reultrasecretofads",
+        "code": "123abdefghy",
+        "category":"Product",
+        "status":true,
+        "id": 3
+      },
+    {
+        "title": "prod4",
+        "stock": 121,
+        "description": "",
+        "price": 100,
+        "thumbnail": "reultrasecretofads",
+        "code": "123abdefghy",
+        "category":"Product",
+        "status":true,
+        "id": 4
+      },
+] 
 
-const products = new prodManag()
 
 const convertToNumber =(req, res ,next)=>{
     req.params.id = Number(req.params.id)
@@ -12,19 +60,64 @@ const convertToNumber =(req, res ,next)=>{
 }
 
 router.get ('/',(req, res)=>{
-    res.json({products})
+
+  const {limit} = req.query
+  let absLimit= Math.abs(limit) 
+  let limitedProds = Product.slice(0, absLimit)
+  if (absLimit) {
+    if (absLimit>Product.length) {
+      return res.send(`<h1>El límite establecido es mayor a la cantidad de productos existentes</h1>`)
+    }
+    return res.send(limitedProds)
+  }
+  res.json(Product)
 })
 
 router.get ('/:id',convertToNumber,(req, res)=>{
-    const id = req.params;
-    res.json({message: id })
+  const {id} = req.params
+  const product =Product.find(product => product.id === parseInt(id)) 
+  if(product == undefined){
+    return res.send(`<h1>El id que colocó, no coincide con ninguno de los productos enlistados</h1>`)
+  }
+  res.json(product)
+
 })
 
 router.post ('/',async(req, res)=>{
-    const infoProduct = req.body
-    products.push(infoProduct)
-    
-    res.json({message: 'Hi products Created'})
+   const {name, description, title, stock, price,id,category,status} = req.body;
+  
+   const product ={name,
+    description,
+    title,
+    stock,
+    price,
+    category,
+    status,
+    id}
+    res.json({message:'prod Created'})
+})
+
+router.put('/', (req, res)=>{
+  
+})
+
+router.delete('/:id',async(req, res)=>{
+  const {id} = req.params
+  const product =Product.find(product => product.id === parseInt(id)) 
+  if(product == undefined){
+    return res.send(`El producto con id ${id} no  se elimino`)
+  }
+  return res.json('El producto se elimino correctamente')
+  
+
+
+
+  try {
+    await Product(req.params.id);
+    res.status(200).send("Product has been deleted...");
+  } catch (error) {
+    res.status(500).send(error);
+  }
 })
 
 
